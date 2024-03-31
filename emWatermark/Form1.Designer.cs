67243c55b3075573b26f6fddfc2ecd4e4c61fe1e
devoo <bingdog00@bingdog.com>
2024-03-31 13:30:09 +0800

    批量给图片添加水印

---
+++ emWatermark/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace emWatermark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "图片文件目录：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输入水印：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 179);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(502, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(502, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "嵌入水印";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 652);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "嵌入水印工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ emWatermark/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emWatermark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string imgPath= textBox1.Text;
+            string watermark= textBox2.Text;
+            if (imgPath !=""&& watermark!="")
+            {
+                try
+                {
+                    DirectoryInfo folder = new DirectoryInfo(imgPath);
+                    foreach (FileInfo file in folder.GetFiles("*.*", SearchOption.AllDirectories))
+                    {
+                        // 加水印
+                        using (var img = Image.FromStream(File.OpenRead($"{imgPath}\\{file}")))
+                        {
+                            using (var graphic = Graphics.FromImage(img))
+                            {
+                                var font = new Font("微软雅黑", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                                var color = Color.FromArgb(128, 255, 255, 255);
+                                var brush = new SolidBrush(color);
+                                var point = new Point(img.Width - 130, img.Height - 50);
+                                graphic.DrawString(watermark, font, brush, point);
+                                if (!Directory.Exists($"{imgPath}\\emWatermark\\"))
+                                    Directory.CreateDirectory($"{imgPath}\\emWatermark\\");
+                                var sdk = img.RawFormat;
+                                img.Save($"{imgPath}\\emWatermark\\{file}", img.RawFormat);
+                            }
+
+                        }
+
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                   MessageBox.Show($"异常信息={ex.Message}");
+                }
+               
+
+
+            }
+            else
+            {
+                MessageBox.Show("目录和水印不可为空！");
+            }
+           
+
+
+        }
+    }
+}
